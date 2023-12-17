2fdf2097754f95907cf53ecae9af8a6c6e24ad38
Mehmet Ali Değirmenci <86076087+malidegirmenci@users.noreply.github.com>
2023-12-18 01:14:49 +0300

    init spring project

---
+++ src/test/java/com/workintech/s17challange/S17challangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.s17challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S17challangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
